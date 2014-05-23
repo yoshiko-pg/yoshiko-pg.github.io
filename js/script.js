ddad89901a826833decd8617e6f10a6a5690dc6c
maasa <maasa.giss2@gmail.com>
2014-05-23 16:09:13 +0200

    retab

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(function(){
-	$('#close').click(function(){
-		if(window.confirm('保存されていません！閉じてもよろしいですか？')){
-			var nvua = navigator.userAgent;
-			if(nvua.indexOf('MSIE') >= 0){
-				if(nvua.indexOf('MSIE 5.0') == -1) {
-					top.opener = '';
-				}
-			}else if(nvua.indexOf('Gecko') >= 0){
-				top.name = 'CLOSE_WINDOW';
-				wid = window.open('','CLOSE_WINDOW');
-			}
-			top.close();
-		}
-	});
+  $('#close').click(function(){
+    if(window.confirm('保存されていません！閉じてもよろしいですか？')){
+      var nvua = navigator.userAgent;
+      if(nvua.indexOf('MSIE') >= 0){
+        if(nvua.indexOf('MSIE 5.0') == -1) {
+          top.opener = '';
+        }
+      }else if(nvua.indexOf('Gecko') >= 0){
+        top.name = 'CLOSE_WINDOW';
+        wid = window.open('','CLOSE_WINDOW');
+      }
+      top.close();
+    }
+  });
 })
