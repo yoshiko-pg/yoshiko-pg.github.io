794a70667cd63aded37d5174248c41a1eb3814bf
maasa <maasa.giss2@gmail.com>
2014-06-03 17:19:53 +0900

    URLアンカーで直リンク

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ $(function(){
   var datas = document.getElementById('navi_marker').dataset;
   var i = 0, j = 0;
   var menu = ['about', 'skills', 'works', 'contact'];
+  var offsets = [];
   var set = function(to, offset){
     $('a[href^=#' + to + ']').click(function() {
       $('html,body').animate({ scrollTop: offset }, 1000);
@@ -38,6 +39,15 @@ $(function(){
   }
   set('home', 0);
   for(key in datas){
-    if(i++%2) set(menu[j++], key);
+    if(i++%2){
+      set(menu[j++], key);
+      offsets.push(key);
+    }
+  }
+
+  // ハッシュで直リンク
+  if(location.hash){
+    var key = menu.indexOf(location.hash.replace('#', ''));
+    $('html,body').animate({ scrollTop: offsets[key] }, 1000);
   }
 });
