aad6a42d6162cf8b06a996b2e222cd5e5597090a
maasa <maasa.giss2@gmail.com>
2014-06-04 09:15:11 +0900

    スマホ対応準備

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,21 @@ var plumber = require('gulp-plumber');
 var prefixer = require('gulp-autoprefixer');
 
 gulp.task('slim', function(){
-  gulp.src('index.slim')
+  gulp.src('*.slim')
     .pipe(plumber())
     .pipe(slim())
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('sass', function(){
-  gulp.src('css/style.scss')
+  gulp.src('css/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(prefixer('last 2 version'))
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', ['slim', 'sass'], function(){
-  //gulp.watch('index.slim', ['slim']);
-  gulp.watch('css/style.scss', ['sass']);
+gulp.task('default', ['slim', 'sass'], function(){
+  gulp.watch('*.slim', ['slim']);
+  gulp.watch('css/*.scss', ['sass']);
 });
