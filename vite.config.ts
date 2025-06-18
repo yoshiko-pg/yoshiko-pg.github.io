cb8760bc9f37780d526cd58d40f0e4822438060f
maasa <maasa.giss2@gmail.com>
2025-06-18 17:43:19 +0900

    動いた

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssNesting from 'postcss-nesting'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        require('postcss-nesting'),
+        postcssNesting(),
       ],
     },
   },
