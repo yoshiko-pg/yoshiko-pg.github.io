0adabaadde2ab7b4166917704fcb3569659537eb
maasa <maasa.giss2@gmail.com>
2025-06-26 03:11:47 +0900

    スクロール位置リセット

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 import TalkDetail from './pages/TalkDetail';
+import ScrollToTop from './components/ScrollToTop';
+
+function Layout() {
+  return (
+    <>
+      <ScrollToTop />
+      <Outlet />
+    </>
+  );
+}
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/talks/:slug',
-    element: <TalkDetail />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'talks/:slug',
+        element: <TalkDetail />,
+      },
+    ],
   },
 ]);
